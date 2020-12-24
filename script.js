5ec1cf4f5bacfb940857f2322a2fa245ab45d895
Gabor Karikas <KariG93@users.noreply.github.com>
2020-12-24 22:37:45 +0100

    files now are in the correct folder

---
+++ script.js
@@ -0,0 +1,23 @@
+let color = "purple"
+let number = 10
+let word = "cool"
+
+if (color === "purple") {
+$('.red').css('background-color', 'purple');
+}
+
+
+if (number > 100) {
+    $('.yellow').text("whoah, that's a big number.");
+}
+else {
+    $('.yellow').text("just a regular number, please.");
+}
+
+
+if (word = "cool") {
+    $('.green').text("Power of DOM");
+}
+else {
+    $('.green').text("Power of DOM");
+}
